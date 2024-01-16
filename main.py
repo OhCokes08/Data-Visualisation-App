96f534ddff1a6f4cf03e14c67e0284b556f58dda
obafemicoker <femiecoker@gmail.com>
2024-01-16 18:28:52 +0000

    Initial commit, main code for the streamlit data vis app

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+
+
+st.title("Countries Happiness Factors")
+xaxis = st.selectbox("Select a metric for the x axis", ("GDP", "Happiness", "Generosity"))
+yaxis = st.selectbox("Select a metric for the y axis", ("GDP", "Happiness", "Generosity"))
+
+df = pd.read_csv("happy.csv")
+
+match xaxis:
+    case "Happiness":
+        xarray = df["happiness"]
+    case "GDP":
+        xarray = df["gdp"]
+    case "Generosity":
+        xarray = df["generosity"]
+match yaxis:
+    case "Happiness":
+        yarray = df["happiness"]
+    case "GDP":
+        yarray = df["gdp"]
+    case "Generosity":
+        yarray = df["generosity"]
+
+st.subheader(f"{xaxis} and {yaxis}")
+
+
+
+figure = px.scatter(x=xarray, y=yarray, labels={"x":xaxis, "y":yaxis})
+st.plotly_chart(figure)
